4c1a2a975649eca0c49fe240e8d1de4598bf4a66
Dimitar-Pilyakov <dimitar.pilyakov@gmail.com>
2019-10-11 11:35:40 +0200

    Adding method for for duplicate receive call.

--- wappsto/connection/communication.py
+++ wappsto/connection/communication.py
@@ -362,18 +362,7 @@ class ClientSocket:
         self.wapp_log.debug("ReceiveThread Started!")
         while True:
             try:
-                total_decoded = []
-                decoded = None
-                while True:
-                    data = self.my_socket.recv(2000)
-                    decoded_data = data.decode('utf-8')
-                    total_decoded.append(decoded_data)
-                    try:
-                        decoded = json.loads(''.join(total_decoded))
-                    except json.decoder.JSONDecodeError:
-                        pass
-                    else:
-                        break
+                decoded = self.receive_data()
                 decoded_id = decoded.get('id')
                 try:
                     self.wapp_log.debug('Raw received Json: {}'
@@ -402,18 +391,15 @@ class ClientSocket:
 
                 except ValueError:
                     self.wapp_log.info("Value error")
-                    self.wapp_log.info(data)
+                    self.wapp_log.info(decoded)
                     error_str = 'Value error'
                     self.send_error(error_str, decoded_id)
 
             except ValueError:
-                if data == b'':
-                    self.reconnect()
-                else:
-                    self.wapp_log.info("Value error")
-                    self.wapp_log.info(data)
-                    error_str = 'Value error'
-                    self.send_error(error_str, decoded_id)
+                self.wapp_log.info("Value error")
+                self.wapp_log.info(decoded)
+                error_str = 'Value error'
+                self.send_error(error_str, decoded_id)
 
             except ConnectionResetError as e:
                 msg = "Received Reset: {}".format(e)
@@ -554,6 +540,40 @@ class ClientSocket:
             msg = "Error sending control: {}".format(e)
             self.wapp_log.error(msg, exc_info=True)
 
+    def receive_data(self):
+        """[summary].
+
+        [description]
+
+        Returns:
+            [description]
+            [type]
+
+        """
+        total_decoded = []
+        decoded = None
+        while True:
+            data = self.my_socket.recv(4000)
+            decoded_data = data.decode('utf-8')
+            total_decoded.append(decoded_data)
+            try:
+                decoded = json.loads(''.join(total_decoded))
+            except json.decoder.JSONDecodeError:
+                pass
+            except ValueError:
+                if decoded == b'':
+                    self.reconnect()
+                else:
+                    self.wapp_log.info("Value error")
+                    self.wapp_log.info(data)
+                    error_str = 'Value error'
+                    decoded_id = decoded.get('id')
+                    self.send_error(error_str, decoded_id)
+            else:
+                break
+
+        return decoded
+
     def send_reconnect(self):
         """
         Send a reconnect attempt.
@@ -676,22 +696,11 @@ class ClientSocket:
             A positive response.
 
         """
-        total_decoded = []
-        decoded = None
-        while True:
-            data = self.my_socket.recv(4000)
-            decoded_data = data.decode('utf-8')
-            total_decoded.append(decoded_data)
-            try:
-                decoded = json.loads(''.join(total_decoded))
-            except json.decoder.JSONDecodeError:
-                pass
-            else:
-                break
+        decoded = self.receive_data()
 
         self.wapp_log.debug("Received after assembly: {}".format(decoded))
 
-        if data == b'':
+        if decoded is None:
             self.wapp_log.info("Server disconnected")
             return False
 
