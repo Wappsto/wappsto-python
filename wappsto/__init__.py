0d24f09501cf67e10f13ba6e7e8bef5d6ef7c03d
Karsten Tonnesen <karsten@seluxit.com>
2019-11-21 10:18:58 +0100

    Release 1.0.10.

--- wappsto/__init__.py
+++ wappsto/__init__.py
@@ -27,7 +27,7 @@ class Wappsto:
     starts a sending/receiving thread.
     """
 
-    __version__ = "1.0.9"
+    __version__ = "1.0.10"
 
     def __init__(self, json_file_name=None, load_from_state_file=False,
                  save_init=False):
