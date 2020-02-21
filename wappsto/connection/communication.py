5f829109436019676fce34520326d1b8619a67c5
Ralf <bubriks@gmail.com>
2020-02-21 12:28:30 +0100

    does not send empty lists or dictionaries (#83)

--- wappsto/connection/communication.py
+++ wappsto/connection/communication.py
@@ -484,10 +484,13 @@ class ClientSocket:
         if self.connected:
             for data_element in data:
                 self.get_object_without_none_values(data_element)
-            data = json.dumps(data)
-            data = data.encode('utf-8')
-            self.wapp_log.debug('Raw Send Json: {}'.format(data))
-            self.my_socket.send(data)
+                if len(data_element) == 0:
+                    data.remove(data_element)
+            if len(data) > 0:
+                data = json.dumps(data)
+                data = data.encode('utf-8')
+                self.wapp_log.debug('Raw Send Json: {}'.format(data))
+                self.my_socket.send(data)
         else:
             self.wapp_log.error('Sending while not connected')
 
@@ -506,9 +509,15 @@ class ClientSocket:
                 del encoded_object[key]
             elif isinstance(val, dict):
                 self.get_object_without_none_values(val)
+                if len(val) == 0:
+                    del encoded_object[key]
             elif isinstance(val, list):
                 for val_element in val:
                     self.get_object_without_none_values(val_element)
+                    if len(val_element) == 0:
+                        val.remove(val_element)
+                if len(val) == 0:
+                    del encoded_object[key]
 
     def send_thread(self):
         """
