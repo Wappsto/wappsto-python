b106acb1683e8a2d40158805d14a71059b770764
Michael Bach Kristensen <michael@seluxit.com>
2020-11-13 13:27:27 +0100

    Bumped Version to v.1.2.4

--- wappsto/__init__.py
+++ wappsto/__init__.py
@@ -38,7 +38,7 @@ class Wappsto:
     Establishes a connection to the wappsto server, initializes the classes,
     starts a sending/receiving thread.
     """
-    __version__ = "1.2.3"
+    __version__ = "1.2.4"
 
     def __init__(self, json_file_name=None, load_from_state_file=False,
                  log_offline=False, log_location="logs",
